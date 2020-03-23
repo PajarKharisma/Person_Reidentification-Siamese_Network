5f66bb82af31f445cdbd5602ee5d39040b44e871
PajarKharisma <pajarkharisma@hotmail.com>
2020-03-23 10:51:46 +0700

    add some file

--- src/dataPreparation/CreateCsv.py
+++ src/dataPreparation/CreateCsv.py
@@ -1,5 +1,12 @@
 import cv2
 import pandas
+import os
 
-def funcname(parameter_list):
-    pass
+def data_contrastive(images_path='', save_path=''):
+    path = 'dataset\\'
+
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(images_path):
+	    for file in f:
+		    print(file)
