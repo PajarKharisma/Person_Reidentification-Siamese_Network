8d4152279eb38237b1a2ee3fb22765554f29e6ea
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-02 16:14:50 +0700

    complete

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@
 #SBATCH --mem=64GB
 #SBATCH --gres=gpu:2                                                   
 #SBATCH --time=20:00:00                           
-#SBATCH --output=result-%j.out    
-#SBATCH --error=result-%j.err
+#SBATCH --output=log/result/result-%j.out    
+#SBATCH --error=log/result/result-%j.err
 
 module load cuda/10.2
-module load tensorrt/6-cuda10.1
-source /<dir_untuk_virtualenv_tf>/bin/activate
+module load tensorrt/6-cuda10.2
+source /.venv/bin/activate
 
 python3 src/main/Main.py
