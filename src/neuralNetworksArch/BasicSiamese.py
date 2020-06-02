541c065db6400ce36a3c1083dec6f69e44fd9a53
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-02 19:40:36 +0700

    fix model for triplet and constrastive

--- src/neuralNetworksArch/BasicSiamese.py
+++ src/neuralNetworksArch/BasicSiamese.py
@@ -48,10 +48,14 @@ class BasicSiameseNetwork(nn.Module):
         output = self.fc1(output)
         return output
 
-    def forward(self, input1, input2):
+    def forward(self, input1, input2, input3=None):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-        return output1, output2
+        if input3 == None:
+            return output1, output2
+        else:
+            output3 = self.forward_once(input3)
+            return output1, output2, output3        
 
     def _initialize_weights(self):
         for m in self.modules():
