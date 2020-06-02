0482062be8aa1e3da1230f8f583ab865d4ceed89
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-02 16:20:54 +0700

    update

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 #SBATCH --ntasks=32               
 #SBATCH --mem=64GB
 #SBATCH --gres=gpu:2                                                   
-#SBATCH --time=20:00:00                           
+#SBATCH --time=30:00:00
 #SBATCH --output=log/result/result-%j.out    
 #SBATCH --error=log/result/result-%j.err
 
-module load cuda/10.2
-module load tensorrt/6-cuda10.2
+module load cuda/10.1
+module load tensorrt/6-cuda10.1
 source .venv/bin/activate
 
 python3 src/main/Main.py
