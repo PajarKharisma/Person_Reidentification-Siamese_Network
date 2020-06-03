c75e540ee162754aa15fb52f8fab4fe30c6625f3
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-04 06:10:13 +0700

    test 5000 data

--- src/main/Main.py
+++ src/main/Main.py
@@ -154,8 +154,9 @@ def contrastive_train():
             optimizer.step()
 
             # get loss and acc train
-            train_loss = loss_contrastive.item()
-            train_acc = metrics.get_acc(output1, output2, label, THRESHOLD)
+            train_loss = (train + loss_contrastive.item()) / iteration
+            train_acc = (train_acc + metrics.get_acc(output1, output2, label, THRESHOLD)) / iteration
+            iteration += 1
 
         if train_loss < best_loss:
             best_loss = train_loss
