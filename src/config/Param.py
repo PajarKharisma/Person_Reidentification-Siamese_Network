1d7c0f992d052e6ceca2a5f2c067d2d50be22bcd
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-08 19:32:59 +0700

    update loss function

--- src/config/Param.py
+++ src/config/Param.py
@@ -2,5 +2,5 @@ import torch
 
 class Param():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    train_batch_size = 32
+    train_batch_size = 128
     train_number_epochs = 10
