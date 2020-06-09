c1f0edb79f3f4689074e57dbded112e81a2114c6
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-09 12:40:33 +0700

    fix acc calculate in triplet

--- src/utils/Metrics.py
+++ src/utils/Metrics.py
@@ -7,11 +7,18 @@ from sklearn.metrics import accuracy_score
 
 from src.config.Param import *
 
-def distance_to_class(distances, threshold=0.5):
-    distances = distances.flatten().detach().cpu().numpy()
-    distances_norm = [abs((1 / (1 + d)) - 1) for d in distances]
-    y = [0.0 if d <= threshold else 1.0 for d in distances_norm]
-
+def distance_to_class(distances, threshold=0.5, margin=1.0, data_type='PAIR'):
+    if data_type == 'PAIR':
+        distances = distances[0].flatten().detach().cpu().numpy()
+        distances_norm = [abs((1 / (1 + d)) - 1) for d in distances]
+        y = [0.0 if d <= threshold else 1.0 for d in distances_norm]
+    else:
+        dist_p = distances[0].flatten().detach().cpu().numpy()
+        dist_n = distances[1].flatten().detach().cpu().numpy()
+        y = []
+        for i in range(len(dist_p)):
+            dist = 0.0 if dist_p[i] + margin <= dist_n[i] else 1.0
+            y.append(dist)
     return np.array(y)
 
 def get_distances(x1, x2):
@@ -20,11 +27,15 @@ def get_distances(x1, x2):
 def get_acc(x1, x2, x3, threshold=0.5, data_type='PAIR'):
     if data_type == 'PAIR':   
         y_true = x3.flatten().cpu().numpy()
+        distances = [get_distances(x1, x2)]
+        y_pred = distance_to_class(distances=distances, threshold=threshold)
     else:
         y_true = np.full((len(x1)), 0.0)
+        dist_a = get_distances(x1,x2)
+        dist_b = get_distances(x1,x3)
+        distances = [dist_a, dist_b]
+        y_pred = distance_to_class(distances=distances, threshold=threshold, data_type=data_type)
     
-    distances = get_distances(x1, x2)
-    y_pred = distance_to_class(distances, threshold)
     return accuracy_score(y_true, y_pred)
 
 def get_val_loss(base_model, loss_func, dataset, data_type='PAIR'):
