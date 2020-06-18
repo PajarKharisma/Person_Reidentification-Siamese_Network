2d0a06a0bb1d16134aa2360e3f09cf038bd94a88
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-18 22:53:49 +0700

    fix bug model gpu

--- src/main/PrintModel.py
+++ src/main/PrintModel.py
@@ -8,6 +8,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchsummary import summary
 
+from src.config.Param import *
+
 import src.neuralNetworksArch.VggArch as vgg
 import src.neuralNetworksArch.BasicSiamese as bSiamese
 import src.neuralNetworksArch.NasnetMobile as nasnet
@@ -16,5 +18,6 @@ import src.neuralNetworksArch.BstCnn as btsCnn
 import src.neuralNetworksArch.McbCnn as mcbCnn
 
 model = mcbCnn.McbCnn()
+model = model.to(Param.device)
 # print(model)
 summary(model, [(3, 60, 160), (3, 60, 160)])
