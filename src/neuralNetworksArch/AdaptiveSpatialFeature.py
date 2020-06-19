e5c28e06d95f6b456bdef0100507e27ae1b421af
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-19 22:38:18 +0700

    rearch adaptive spatial

--- src/neuralNetworksArch/AdaptiveSpatialFeature.py
+++ src/neuralNetworksArch/AdaptiveSpatialFeature.py
@@ -37,17 +37,13 @@ class AdaptiveSpatialFeature(nn.Module):
         self.fc1 = nn.Sequential(
             nn.Linear(64*15*40, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(p=0.2),
             
             nn.Linear(4096, 2048),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(p=0.2),
             
             nn.Linear(2048, 1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            
-            nn.Linear(1024, 512),
             nn.Sigmoid()
         )
 
