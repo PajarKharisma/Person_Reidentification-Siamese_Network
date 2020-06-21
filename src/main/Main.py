73f62371d180e86e2729947c94937de385be20ff
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-21 15:16:12 +0700

    train mcb

--- src/main/Main.py
+++ src/main/Main.py
@@ -176,7 +176,7 @@ def training(model, loss_function, dataset, data_type):
     torch.save(best_model.state_dict(), Path.model)
 
 def contrastive_train():
-    model = bst.BstCnn()
+    model = mcb.McbCnn()
     model.to(Param.device)
 
     criterion = lossFunc.ContrastiveLoss()
