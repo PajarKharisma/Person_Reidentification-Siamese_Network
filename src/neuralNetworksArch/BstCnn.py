d1af3881ae13686ed0459263217a08a060099ab4
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-25 17:01:34 +0700

    train cuhk03

--- src/neuralNetworksArch/BstCnn.py
+++ src/neuralNetworksArch/BstCnn.py
@@ -40,10 +40,10 @@ class BstCnn(nn.Module):
 
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((15, 40))
+        self.avgpool = nn.AdaptiveAvgPool2d((32, 16))
 
         self.fc = nn.Sequential(
-            nn.Linear(16*15*40, 4096),
+            nn.Linear(16*32*16, 4096),
             nn.ReLU(inplace=True),
             # nn.Dropout(p=0.2),
             
