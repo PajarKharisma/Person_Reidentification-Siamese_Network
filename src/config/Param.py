d121f9304bca9bd7c8cb9c83111b97d1d3935a82
PajarKharisma <pajarkharisma@hotmail.com>
2020-07-01 22:23:27 +0700

    triplet train

--- src/config/Param.py
+++ src/config/Param.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Param():
     #Hyperparameter
-    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     train_batch_size = 32
     train_number_epochs = 10
     data_split = 0.8
