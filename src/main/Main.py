a410f5cd1934492390e1be8a4a95914f1582ea4d
PajarKharisma <pajarkharisma@hotmail.com>
2020-07-02 01:47:11 +0700

    train mcb

--- src/main/Main.py
+++ src/main/Main.py
@@ -193,7 +193,8 @@ def training(model, loss_function, dataset, optimizer, loss, epoch_number=0):
 
 def contrastive_train():
     # model = bst.BstCnn()
-    model = asf.AdaptiveSpatialFeature()
+    # model = asf.AdaptiveSpatialFeature()
+    model = mcb.McbCnn()
     model = model.to(Param.device)
 
     optimizer = optim.Adam(model.parameters(), lr=0.0005)
