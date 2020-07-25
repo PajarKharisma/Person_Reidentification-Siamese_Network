8c951294c5974296f12ce98cb051f3b0e9dc25e2
PajarKharisma <pajarkharisma@hotmail.com>
2020-07-26 00:16:33 +0700

    roc curve

--- src/utils/Metrics.py
+++ src/utils/Metrics.py
@@ -110,6 +110,6 @@ def get_roc_auc(model, dataset):
     
     acc = accuracy_score(y_true, y_pred)
 
-    print('FPR : {}'.format(fpr.shape))
-    print('TPR : {}'.format(tpr.shape))
+    print('y_true : {}'.format(y_true))
+    print('y_score : {}'.format(y_scores))
     return threshold, acc, (fpr, tpr)
