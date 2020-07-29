1c0c443b402382c10bc715c882f3596326b9a8a0
PajarKharisma <pajarkharisma@hotmail.com>
2020-07-29 13:00:46 +0700

    train contrastive loss margin=3.0

--- src/config/Param.py
+++ src/config/Param.py
@@ -12,6 +12,6 @@ class Param():
     input_size = (64,128)
     pretrained = False
     data_type = 'PAIR'
-    desc = 'Train contrastive Loss margin 2 ' + str(datetime.datetime.now())
+    desc = 'Train contrastive Loss margin 3 ' + str(datetime.datetime.now())
 
     threshold = -1
