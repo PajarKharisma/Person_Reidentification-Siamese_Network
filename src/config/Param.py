a87396c2ede84c3b3a972ed12ceaff7d0d60bbaa
PajarKharisma <pajarkharisma@hotmail.com>
2020-09-09 15:09:45 +0700

    train batch size 64

--- src/config/Param.py
+++ src/config/Param.py
@@ -6,13 +6,13 @@ import numpy as np
 class Param():
     #Hyperparameter
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    train_batch_size = 32
+    train_batch_size = 64
     train_number_epochs = 20
     data_split = 0.8
     learning_rate = 0.1
     input_size = (64,128)
     pretrained = False
     data_type = 'PAIR'
-    desc = 'Train Batch Size 32 ' + str(datetime.datetime.now())
+    desc = 'Train Batch Size 64 ' + str(datetime.datetime.now())
 
     threshold = -1
