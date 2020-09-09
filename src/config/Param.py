79c68bb6f9225b271aed2078e2540b78b0724f99
PajarKharisma <pajarkharisma@hotmail.com>
2020-09-09 17:04:25 +0700

    train epoch 50

--- src/config/Param.py
+++ src/config/Param.py
@@ -7,12 +7,12 @@ class Param():
     #Hyperparameter
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     train_batch_size = 32
-    train_number_epochs = 25
+    train_number_epochs = 50
     data_split = 0.8
     learning_rate = 0.1
     input_size = (64,128)
     pretrained = False
     data_type = 'PAIR'
-    desc = 'Train Epoch 25 ' + str(datetime.datetime.now())
+    desc = 'Train Epoch 50 ' + str(datetime.datetime.now())
 
     threshold = -1
